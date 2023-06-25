c1c0797997dc48e7ce8ef23c3c129fa2f32a18d8
Rain <chanhoyeung92@gmail.com>
2023-06-26 04:21:57 +0800

    626

--- functions.js
+++ functions.js
@@ -1,33 +1,122 @@
 let ipt1 = document.getElementById("ipt1")
 let btn1 = document.getElementById("btn1")
 let btn2 = document.getElementById("btn2")
-
+const chatContainer = document.querySelector(".chatContainer")
+const datetime = new Date()
 // const APILINK = "http://127.0.0.1:8000/api/v1/rtchat/";
 const APILINK = "http://localhost:8000/api/v1/rtchat/"
 
 function SaveUsername() {
-    console.log('saving username...', ipt1.value)
-    const username = ipt1.value;
+  let username = ipt1.value;
+  localStorage.setItem("user_name_rcweb", username)
+}
+
+function GetMessages() {
+  fetch(APILINK+"getmsg",
+  )
+  .then(res => res.json())
+    .then(function(data){
+    data.forEach(element => {
+      // console.log(element);
+      returnMsg(element.uniqueId, element.username, element.message, element.sentTime);
+    });
+  });
+}
+
+function formatMsg(uniqueId, username, msg, time) {
+    //custom
+    if (username == 'Shirley') {
+      name_color = '#C5FFDC';
+    } else {
+      name_color = 'pink';
+    }
+    return (
+      `
+          <div class="chat">
+            <div class="time" style="color:#DDE2D8">${time}</div>
+            <div class="${username} name" style="color: ${name_color}">${username}:
+              </div>
+              <div class="message" id=${uniqueId}>${msg}</div>
+          </div>
+      `
+     )
+     
+  }
+
+  function returnMsg(uniqueId, username, message, currentTime) {
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+    chatContainer.innerHTML += formatMsg(uniqueId, username, message, currentTime)
+  }
+  
+  function OutputMsg() {
+    let username;
+    let message = ipt1.value;
+    let uniqueId = generateUniqueId();
+    let currentTime = getDate();
+    if (localStorage.getItem("user_name_rcweb") == null) {
+      ipt1.textContent = "Please name yourself first ^^, Thank you :)"
+      return
+    } else {
+      username = localStorage.getItem("user_name_rcweb")
+    }
 
     fetch(APILINK + "new", {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-            // "Access-Control-Allow-Origin" : "*", 
-            // "Access-Control-Allow-Credentials" : true
-        },
-        body: JSON.stringify({"username": username})
-    }).then(res => {
-        console.log(JSON.stringify({"username": username}))
-        if (res.status == 200) return res.json();
-        else{
-            console.log('no response')
-            return Promise.reject(res);
-        };
-    })
-        .then(res => {
-        console.log(res);
-        location.reload();
-    })
-}
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+          // "Access-Control-Allow-Origin" : "*", 
+          // "Access-Control-Allow-Credentials" : true
+      },
+      body: JSON.stringify({"uniqueId": uniqueId, "username": username, "message": message, "sentTime": currentTime})
+  }).then(res => {
+      if (res.status == 200) {
+        // console.log(res)
+        return res.json();}
+      else{
+          console.log('no response')
+          return Promise.reject(res);
+      };
+  })
+      .then(res => {
+      // console.log(res);
+      // location.reload();
+  })
+  // main ? '' : sendNoti(); 
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  chatContainer.innerHTML += formatMsg(uniqueId, username, message, currentTime)
+  ipt1.value = '';
+  }
+  
+  function getDate() {
+    let month = datetime.getMonth();
+    let hour = String(datetime.getHours()).padStart(2, '0');
+    let min = String(datetime.getMinutes()).padStart(2, '0');
+    return hour+":"+min;
+  }
+  
+  function enterUsername() {
+    username = ipt1.value;
+    ipt1.value = '';
+  
+    localStorage.setItem("rains_uname", username);
+  }
+
+  function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+  
+    return `id-${timestamp}-${hexadecimalString}`;
+  }
+
+  function getDate() {
+    let month = datetime.getMonth();
+    let hour = String(datetime.getHours()).padStart(2, '0');
+    let min = String(datetime.getMinutes()).padStart(2, '0');
+    return hour+":"+min;
+  }
+
+
+  GetMessages();
---
+++ real-time.js
@@ -0,0 +1,179 @@
+
+//references
+var ipt1 = document.getElementById("ipt1")
+var btn1 = document.getElementById("btn1")
+var btn2 = document.getElementById("btn2")
+var notificationBtn = document.getElementById("notiBtn")
+const chatContainer = document.querySelector(".chatContainer")
+const datetime = new Date()
+
+//username storage
+var username = localStorage.getItem("rains_uname");
+var recname, recmsg, recid, name_color, currentTime;
+var main = false; //bool, to determine if the message send by the own user
+
+var uniqueId = generateUniqueId();
+
+
+
+//insert data function
+function InsertData() {
+
+  set(ref(db, "Chat"), {
+    name: username,
+    msg: ipt1.value,
+    id: uniqueId,
+    time: getDate()
+  })
+  .then(() => {
+  })
+  .catch((err) => {
+    alert("Error occured: " + err)
+  });
+
+  main = true;
+
+}
+
+//listen for incoming message
+const dbref = ref(db, 'Chat')
+
+onValue(dbref, (snapshot) => {
+  if(snapshot.exists()) {
+    recname = snapshot.val().name;
+    recmsg = snapshot.val().msg;
+    recid = snapshot.val().id;
+    currentTime = snapshot.val().time;
+    outputMsg();
+  }})
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function formatMsg(uniqueId, username, msg, time) {
+  //custom
+  if (username == 'Shirley') {
+    name_color = '#C5FFDC';
+  } else {
+    name_color = 'pink';
+  }
+  return (
+    `
+        <div class="chat">
+          <div class="time" style="color:#DDE2D8">${time}</div>
+          <div class="${username} name" style="color: ${name_color}">${username}:
+            </div>
+            <div class="message" id=${uniqueId}>${msg}</div>
+        </div>
+    `
+   )
+   
+}
+
+const outputMsg = async() => {
+
+  uniqueId = generateUniqueId()
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  chatContainer.innerHTML += formatMsg(uniqueId, recname, recmsg, currentTime)
+  
+  ipt1.value = '';
+  main ? '' : sendNoti(); 
+}
+
+function getDate() {
+  let month = datetime.getMonth();
+  let hour = String(datetime.getHours()).padStart(2, '0');
+  let min = String(datetime.getMinutes()).padStart(2, '0');
+  return hour+":"+min;
+}
+
+function enterUsername() {
+  username = ipt1.value;
+  ipt1.value = '';
+
+  localStorage.setItem("rains_uname", username);
+}
+
+//notification
+function checkNotificationPromise() {
+  try {
+    Notification.requestPermission().then();
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
+
+function askNotificationPermission() {
+  // // function to actually ask the permissions
+  // function handlePermission(permission) {
+  //   // set the button to shown or hidden, depending on what the user answers
+  //   notificationBtn.style.display =
+  //     Notification.permission === 'granted' ? 'none' : 'block';
+  // }
+
+  // Let's check if the browser supports notifications
+  if (!('Notification' in window)) {
+    console.log("This browser does not support notifications.");
+  } else if (checkNotificationPromise()) {
+    Notification.requestPermission().then((permission) => {
+      console.log("success")
+      // handlePermission(permission);
+    });
+  } else {
+    Notification.requestPermission((permission) => {
+      // handlePermission(permission);
+      console.log("success")
+    });
+  }
+}
+
+
+function sendNoti() {
+  const img = '/img/xuan1.jpg';
+  const text = `${recname}: ${recmsg}`;
+
+  navigator.serviceWorker.register('sw.js');
+
+  if (Notification?.permission === "granted") {
+      // If the user agreed to get notified
+      // Let's try to send ten notifications
+      // const notification = new Notification('', {body: text, icon: img });
+      navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification('', {body: text, icon: img });
+      });
+    } else if (Notification && Notification.permission !== "denied") {
+      // If the user hasn't told if they want to be notified or not
+      // Note: because of Chrome, we are not sure the permission property
+      // is set, therefore it's unsafe to check for the "default" value.
+      Notification.requestPermission((status) => {
+        // If the user said okay
+        if (status === "granted") {
+          // const notification = new Notification('', { body: text, icon: img });
+          navigator.serviceWorker.ready.then(function(registration) {
+            registration.showNotification('', {body: text, icon: img });
+          });
+        }})
+      }
+
+    }
+
+askNotificationPermission();
+
+btn2.addEventListener('click', enterUsername);
+
+btn1.addEventListener('click', InsertData);
+ipt1.addEventListener('keyup', function(e) {
+  if(e.keyCode === 13) {
+    e.preventDefault();
+    btn1.click();
+
+  }
+})
